d1c8054186a2028ef25fcf45fd8455e6126b9963
sh <tshddo@outlook.com>
2026-03-15 17:38:47 -0400

    load

--- LLM/load.py
+++ LLM/load.py
@@ -1,43 +1,103 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from collections import defaultdict
+import numpy as np
+import random
+import os
+import torch.fx as fx
+import inspect
 
-model_name = "mistralai/Mistral-7B-v0.1"
-# alternatives:
+
+seed = 29
+    
+# set random seed
+random.seed(seed)
+os.environ['PYTHONHASHSEED'] = str(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '1' 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+# model_name = "mistralai/Mistral-7B-v0.1"
 # model_name = "meta-llama/Meta-Llama-3-8B"
-# model_name = "Qwen/Qwen2-7B"
+model_name = "Qwen/Qwen2.5-7B-Instruct"
 
 print("Loading model:", model_name)
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    torch_dtype=torch.float16,
+    dtype=torch.float16,
     device_map="cpu"
 )
 
+model.to(device)
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-print("\n===== MODEL ARCHITECTURE =====\n")
-print(model)
+# print("\n===== MODEL ARCHITECTURE =====\n")
+# print(model)
 
-print("\n===== PARAMETER SUMMARY =====\n")
+# model_n = model_name.split('/')[1]
 
-total_params = 0
-layer_params = defaultdict(int)
+# with open(model_n + ".txt", "w+") as f:
+#     # traced = fx.symbolic_trace(model.model.layers[0])
+#     layer = model.model.layers[0]
+#     f.write("\n===== execution order =====\n")
+#     f.write(f'{inspect.getsource(layer.forward)}\n')
+#     f.write("\n===== attention execution order =====\n")
+#     f.write(f'{inspect.getsource(layer.self_attn.forward)}\n')
+#     f.write("\n===== mlp execution order =====\n")
+#     f.write(f'{inspect.getsource(layer.mlp.forward)}\n')
+    
+    
 
-for name, param in model.named_parameters():
-    num = param.numel()
-    total_params += num
+# print("\n===== PARAMETER SUMMARY =====\n")
 
-    # group by top-level module
-    layer_name = name.split('.')[0]
-    layer_params[layer_name] += num
+# total_params = 0
+# layer_params = defaultdict(int)
 
-    print(f"{name:60} {num/1e6:8.2f} M")
+# for name, param in model.named_parameters():
+#     num = param.numel()
+#     total_params += num
 
-print("\n===== PARAMS PER TOP MODULE =====")
+#     # group by top-level module
+#     layer_name = name.split('.')[0]
+#     layer_params[layer_name] += num
 
-for k,v in layer_params.items():
-    print(f"{k:20} {v/1e6:.2f} M")
+#     print(f"{name:60} {num/1e6:8.2f} M")
 
-print("\nTOTAL PARAMETERS:", total_params/1e9, "B")
+# print("\n===== PARAMS PER TOP MODULE =====")
+
+# for k,v in layer_params.items():
+#     print(f"{k:20} {v/1e6:.2f} M")
+
+# print("\nTOTAL PARAMETERS:", total_params/1e9, "B")
+
+
+prompt = "Hello"
+messages = [
+    {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=512
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(response)
